a8f6d7f8b921ddbc1a408046c231a39ba1875a0f
LHUMCohen <LHUMCohen@cbc059f5-088e-1f8e-81ed-558d72521801>
2010-09-12 17:30:25 +0000

    Initial version published in ACM Crossroads 2005.

---
+++ oogt/v1.0/src/edu/lhup/ai/tictactoe/XPiece.java
@@ -0,0 +1,21 @@
+package edu.lhup.ai.tictactoe;
+
+import edu.lhup.ai.*;
+
+/**
+ * Represents a location on the {@link Board tic-tac-toe} board that contains 
+ * an X. 
+ *
+ * <p>
+ * This software is for educational purposes only.
+ * @author Mark Cohen 
+ */
+class XPiece implements IPiece
+{
+	XPiece() {}
+
+	public String toString()
+	{
+		return "X";
+	}
+}
